24d86d4c61b34463abdd32d438d308aadd707252
Sivaaa <periyasamy3337@gmail.com>
2026-09-03 19:52:30 +0530

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,6 +8,7 @@ android {
     namespace = "com.carromarena.app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
+
     defaultConfig {
         applicationId = "com.carromarena.app"
         minSdk = 24
@@ -15,6 +16,18 @@ android {
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
+
+    // Fix Java and Kotlin JVM target mismatch
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
 
-flutter { source = "../.." }
+flutter {
+    source = "../.."
+}
