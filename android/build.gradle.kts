c99f00a3055069e81b99db28709e32f94d7ed79c
Sivaaa <periyasamy3337@gmail.com>
2026-09-03 19:51:17 +0530

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,5 +1,26 @@
-allprojects { repositories { google(); mavenCentral() } }
-val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
+import org.gradle.api.file.Directory
+import org.gradle.api.tasks.Delete
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+val newBuildDir: Directory =
+    rootProject.layout.buildDirectory
+        .dir("../../build")
+        .get()
+
 rootProject.layout.buildDirectory.value(newBuildDir)
-subprojects { project.layout.buildDirectory.value(newBuildDir.dir(project.name)) }
-tasks.register<Delete>("clean") { delete(rootProject.layout.buildDirectory) }
+
+subprojects {
+    project.layout.buildDirectory.value(
+        newBuildDir.dir(project.name)
+    )
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
+}
